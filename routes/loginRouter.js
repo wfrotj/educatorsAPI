497a1cdbf45dc27ccfb8b3cc1a7d241f296e9476
wfrotj <trebrodrigo@gmail.com>
2024-01-30 00:44:19 +0800

    edit routes

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -5,6 +5,5 @@ import loginController from "../controllers/loginController.js";
 const loginRouter = express.Router();
 
 loginRouter.post("/", loginController.login);
-// loginRouter.post("/masterteacher", loginController.mtlogin);
 
 export default loginRouter;
